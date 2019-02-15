30ad0e1db2330703f971e6eae95652ac034fdbdd
msftrncs <msftrncs@midlands.net>
2019-02-15 01:28:39 -0600

    overhaul to ConvertFrom-Plist, internal class based converter

--- PList Reader.ps1
+++ PList Reader.ps1
@@ -1,95 +1,101 @@
 # parameter to pass input via pipeline
-Param(
-    [Parameter(Mandatory = $true,
-        Position = 0,
-        ValueFromPipeline = $true,
-        ValueFromPipelineByPropertyName = $true,
-        HelpMessage = "XML Plist object.")]
-    [ValidateNotNullOrEmpty()]
-    [xml]$plist
-)
+function ConvertFrom-Plist {
+    Param(
+        [Parameter(Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "XML Plist object.")]
+        [ValidateNotNullOrEmpty()]
+        [xml]$plist
+    )
 
-function processTree ($node) {
-    $currnode = $node # start at the first node of the node set provided
-    $collection = [PSCustomObject]@{}
+    class plistreader {
+        static [object] processTree ($node) {
+            $currnode = $node # start at the first node of the node set provided
+            $collection = [ordered]@{}
 
-    <# iterate through the collection of XML nodes provided, recursing through the children nodes to
-       extract properties and their values, dictionaries, or arrays of all, but note that property values
-       follow their key, not contained within them, and such retrieving the value requires recursing with a 
-       clone of the next node.
-    #>
-    do {
-        if ($currnode.HasChildNodes) {
-            if ($currnode.Name -eq 'key') {
-                # a key in a dictionary, or a single property, either way, add it to a collection
-                $collection | Add-member -MemberType NoteProperty -name (processTree $currnode.FirstChild) -value (processTree $currnode.NextSibling.CloneNode($true))
-                $currnode = $currnode.NextSibling # skip the next sibling because it was the value of the property
-            }
-            elseif ($currnode.Name -in 'string', 'dict') {
-                # for string, or dict; return the value, with possible recursion and collection
-                processTree $currnode.FirstChild
-            }
-            elseif ($currnode.Name -in 'array' ) {
-                # for arrays, recurse the tree, and always return the array 
-                (, @(processTree $currnode.FirstChild))
-            }
-            elseif ($currnode.Name -eq 'integer') {
-                # must be an integer type value element, return its value
-                (processTree $currnode.FirstChild) | ForEach-Object {
-                    # try to determine what size of interger to return this value as 
-                    if ([int]::TryParse( $_, [ref] $null)) {
-                        # a 32bit integer seems to work
-                        $_ -as [int]
+            <# iterate through the collection of XML nodes provided, recursing through the children nodes to
+            extract properties and their values, dictionaries, or arrays of all, but note that property values
+            follow their key, not contained within them, and such retrieving the value requires recursing with a 
+            clone of the next node.
+            #>
+            do {
+                if ($currnode.HasChildNodes) {
+                    if ($currnode.Name -eq 'key') {
+                        # a key in a dictionary, or a single property, either way, add it to a collection
+                        $collection += [ordered]@{ [plistreader]::processTree($currnode.FirstChild) = [plistreader]::processTree($currnode.NextSibling.CloneNode($true)) }
+                        $currnode = $currnode.NextSibling # skip the next sibling because it was the value of the property
+                    }
+                    elseif ($currnode.Name -eq 'string') {
+                        # for string, or dict; return the value, with possible recursion and collection
+                        return [plistreader]::processTree($currnode.FirstChild)
+                    }
+                    elseif ($currnode.Name -eq 'dict') {
+                        # for string, or dict; return the value, with possible recursion and collection
+                        return [plistreader]::processTree($currnode.FirstChild)
+                    }
+                    elseif ($currnode.Name -eq 'array' ) {
+                        # for arrays, recurse the tree, and always return the array 
+                        return @($(foreach($sibling in $currnode.ChildNodes) {[plistreader]::processTree($sibling.CloneNode($true))}))
+                    }
+                    elseif ($currnode.Name -eq 'integer') {
+                        # must be an integer type value element, return its value
+                        return [plistreader]::processTree($currnode.FirstChild) | ForEach-Object {
+                            # try to determine what size of interger to return this value as 
+                            if ([int]::TryParse( $_, [ref] $null)) {
+                                # a 32bit integer seems to work
+                                $_ -as [int]
+                            }
+                            else {
+                                if ([int64]::TryParse( $_, [ref] $null)) {
+                                    # a 64bit integer seems to be needed
+                                    $_ -as [int64]
+                                }
+                                else {
+                                    # try an unsigned 64bit interger, the largest available here.
+                                    $_ -as [uint64]
+                                }
+                            }
+                        }
+                    }
+                    elseif ($currnode.Name -eq 'real') {
+                        # must be a floating type value element, return its value
+                        return [plistreader]::processTree($currnode.FirstChild) -as [double]
+                    }
+                    elseif ($currnode.Name -eq 'date') {
+                        # must be a date-time type value element, return its value
+                        return [plistreader]::processTree($currnode.FirstChild) -as [datetime]
+                    }
+                    elseif ($currnode.Name -eq 'data') {
+                        # must be a data block value element, return its value as [byte[]]
+                        return [convert]::FromBase64String([plistreader]::processTree($currnode.FirstChild))
                     }
                     else {
-                        if ([int64]::TryParse( $_, [ref] $null)) {
-                            # a 64bit integer seems to be needed
-                            $_ -as [int64]
-                        }
-                        else {
-                            # try an unsigned 64bit interger, the largest available here.
-                            $_ -as [uint64]
-                        }
+                        # we didn't recognize the element type!
+                        throw "Unhandled PLIST type '$($currnode.Name)'!"
                     }
                 }
-            }
-            elseif ($currnode.Name -eq 'real') {
-                # must be a floating type value element, return its value
-                (processTree $currnode.FirstChild) -as [double]
-            }
-            elseif ($currnode.Name -eq 'date') {
-                # must be a date-time type value element, return its value
-                (processTree $currnode.FirstChild) -as [datetime]
-            }
-            elseif ($currnode.Name -eq 'data') {
-                # must be a data block value element, return its value as [byte[]]
-                [convert]::FromBase64String((processTree $currnode.FirstChild))
-            }
-            else {
-                # we didn't recognize the element type!
-                throw "Unhandled PLIST type '$($currnode.Name)'!"
-            }
+                else {
+                    # return simple element value (need to check for Boolean datatype, and process value accordingly)
+                    if ($currnode.Name -eq 'true') {
+                        return $true # return a Boolean TRUE value
+                    }
+                    elseif ($currnode.Name -eq 'false') {
+                        return $false # return a Boolean FALSE value
+                    }
+                    else {
+                        # return the element value
+                        return $currnode.Value
+                    }
+                }
+                $currnode = $currnode.NextSibling # move forward to next node.
+            } until ($null -eq $currnode) # until no more nodes are left in this set
+            # if we built a collection of keys, we need to return the collection, count of object properties doesn't exist until a property is added.
+            return $collection
         }
-        else {
-            # return simple element value (need to check for Boolean datatype, and process value accordingly)
-            if ($currnode.Name -eq 'true') {
-                $true # return a Boolean TRUE value
-            }
-            elseif ($currnode.Name -eq 'false') {
-                $false # return a Boolean FALSE value
-            }
-            else {
-                # return the element value
-                $currnode.Value
-            }
-        }
-        $currnode = $currnode.NextSibling # move forward to next node.
-    } until ($null -eq $currnode) # until no more nodes are left in this set
-    # if we built a collection of keys, we need to return the collection, count of object properties doesn't exist until a property is added.
-    if ($collection.PSObject.Properties.count) {
-        $collection
     }
-}
 
-# process the 'plist' item of the input XML object
-processTree $plist.item("plist").FirstChild
+    # process the 'plist' item of the input XML object
+    [plistreader]::processTree($plist.item("plist").FirstChild)
+}
